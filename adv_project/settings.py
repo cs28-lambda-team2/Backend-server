ee3d9e161499c84a378ecf167172ba781c71d994
Vanessa <vanessatellez45@gmail.com>
2020-04-28 17:53:25 -0400

    changed settings to nonlocal database

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -86,18 +86,18 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 # Use this config for your local sqlite DB for development and testing, comment out when using PostgresQL on heroku
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 
 # Use this config for your deployed PostgresQL DB, comment out when developing or testing locally with sqlite
-# DATABASES = {
-#    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
-# }
+DATABASES = {
+    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
+}
 
 
 # Password validation
